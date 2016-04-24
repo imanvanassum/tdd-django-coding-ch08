3bf599a3c66872790e77eb53505ff47d194059cb
Iman van Assum <imanvanassum@live.nl>
2016-04-25 01:32:57 +0200

    No clue what the unit test's problem is with the HTML comparison. Will figure it out later...

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	def tearDown(self):
 		self.browser.quit()
+
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
@@ -36,7 +37,7 @@ class NewVisitorTest(unittest.TestCase):
 
 	    # When she hits enter, the page updates, and now the page lists "1. Buy peacock feathers" as an item in a to-do list
 	    inputbox.send_keys(Keys.ENTER)
-	    self.check_for_row_in_list_table('1. Buy peacock feathers')
+	    self.check_for_row_in_list_table('1: Buy peacock feathers')
 	    
 	    # There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly" (Edith is very methodical)
 	    inputbox = self.browser.find_element_by_id('id_new_item')
@@ -46,7 +47,7 @@ class NewVisitorTest(unittest.TestCase):
 	    #self.fail('Finish the test!')
 
 	    # The page updates again, and now shows both items on her list
-	    self.check_for_row_in_list_table('1. Buy peacock feathers')
+	    self.check_for_row_in_list_table('1: Buy peacock feathers')
 	    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 	    # Edith wonders whether the site will remember her list. Then she sees that the site has generated a unique URL for her -- there is some explanatory text to that effect.
