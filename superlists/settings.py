7bff8482cc2577e313a60be34bc7cf330eea3947
Iman van Assum <imanvanassum@live.nl>
2021-11-10 23:56:33 +0100

    Left secret key in by accident, oops

--- superlists/settings.py
+++ superlists/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1qt&cm2jz@$)_h=s8l_bhawe0f9$b+)k@s$u(qn=9b53z)!x4+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
